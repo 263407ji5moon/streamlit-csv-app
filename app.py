19036433ffa88be4dc48816aa6aaae548e216519
263407ji5moon <263407@naegok.ms.kr>
2026-04-16 13:26:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -45,14 +45,11 @@ if uploaded_file is not None:
             st.subheader("📊 그래프 생성")
 
             selected_column = st.selectbox("컬럼 선택", numeric_columns)
-            selected_graph_type = st.selectbox("그래프 선택", ["히스토그램", "꺾은선 그래프"]);
+            selected_graph_type = st.selectbox("그래프 선택", ["막대 그래흐", "꺾은선 그래프"]);
             if selected_graph_type == "히스토그램":
                 # 히스토그램
                 st.write("히스토그램")
-                fig, ax = plt.subplots()
-                ax.hist(df[selected_column], bins=20)
-                ax.set_title(f"{selected_column} 분포")
-                st.pyplot(fig)
+                st.bar_chart(df[selected_column])
             if selected_graph_type == "꺾은선 그래프":
                 # 꺾은선 그래프
                 st.write("꺾은선 그래프")
